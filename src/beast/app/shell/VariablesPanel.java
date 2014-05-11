b01b4ff101fca3ff7342c49edbc66573d2ff7fb1
rbouckaert <higgs.ml@gmail.com>
2014-05-11 17:39:47 +1200

    checkpoint

--- src/beast/app/shell/VariablesPanel.java
+++ src/beast/app/shell/VariablesPanel.java
@@ -5,56 +5,146 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextPane;
 
+import beast.app.shell.treetable.JTreeTable;
+import beast.app.shell.treetable.TreeTableModel;
 import bsh.UtilEvalError;
 import bsh.Variable;
 import javax.swing.JTable;
+import javax.swing.event.TreeModelListener;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.tree.TreePath;
 
 public class VariablesPanel extends JSplitPane {
 	private static final long serialVersionUID = 1L;
 
-	private JTable table;
+	private JTreeTable table;
 	JScrollPane scrollPane;
 	Object[][] data;
-	AbstractTableModel model;
+	TreeTableModel model;
 	JTextPane textPane;
 	
 	public VariablesPanel() {
 		super(JSplitPane.VERTICAL_SPLIT);
 		data = new Object[0][2];
-		model = new AbstractTableModel() {
-			private static final long serialVersionUID = 1L;
-
+		model = new TreeTableModel() {
+			
 			@Override
-			public Object getValueAt(int rowIndex, int columnIndex) {
-				return data[rowIndex][columnIndex];
+			public void valueForPathChanged(TreePath path, Object newValue) {
+				// TODO Auto-generated method stub
+				
 			}
 			
 			@Override
-			public int getRowCount() {
-				return data.length;
+			public void removeTreeModelListener(TreeModelListener l) {
+				// TODO Auto-generated method stub
+				
 			}
 			
 			@Override
-			public int getColumnCount() {
-				return 2;
+			public boolean isLeaf(Object node) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public Object getRoot() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public int getIndexOfChild(Object parent, Object child) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			@Override
+			public int getChildCount(Object parent) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			@Override
+			public Object getChild(Object parent, int index) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void addTreeModelListener(TreeModelListener l) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void setValueAt(Object aValue, Object node, int column) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public boolean isCellEditable(Object node, int column) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public Object getValueAt(Object node, int column) {
+				// TODO Auto-generated method stub
+				return null;
 			}
 			
 			@Override
 			public String getColumnName(int column) {
-				if (column == 0) {
-					return "name";
-				}
-				return "value";
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public int getColumnCount() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			@Override
+			public Class<?> getColumnClass(int column) {
+				// TODO Auto-generated method stub
+				return null;
 			}
 		};
+//		() {
+//			private static final long serialVersionUID = 1L;
+//
+//			@Override
+//			public Object getValueAt(int rowIndex, int columnIndex) {
+//				return data[rowIndex][columnIndex];
+//			}
+//			
+//			@Override
+//			public int getRowCount() {
+//				return data.length;
+//			}
+//			
+//			@Override
+//			public int getColumnCount() {
+//				return 2;
+//			}
+//			
+//			@Override
+//			public String getColumnName(int column) {
+//				if (column == 0) {
+//					return "name";
+//				}
+//				return "value";
+//			}
+//		};
 		
-		table = new JTable(model);
+		table = new JTreeTable(model);
+		
 		add(table);
 		scrollPane = new JScrollPane(table);
 		add(scrollPane);
